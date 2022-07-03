2d917b574f82d9b7d8197fb6b4bd3922977a0a24
mamsdeveloper <belk1naalyona@yandex.ru>
2022-07-03 15:18:28 +0600

    43.py solved

---
+++ 43.py
@@ -0,0 +1,12 @@
+"""
+Question:
+Write a program which accepts a string as input to print("Yes" if the string is "yes" or "YES" or "Yes", otherwise print("No". )
+
+"""
+
+
+s = input()
+if s == "yes" or s == "YES" or s == "Yes":
+    print("Yes")
+else:
+    print("No")
