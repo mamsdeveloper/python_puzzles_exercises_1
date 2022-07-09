eca257ccadaa18d175480b70d1cd88ddb612b614
mamsdeveloper <belk1naalyona@yandex.ru>
2022-07-10 01:39:04 +0600

    47.py solved

---
+++ 47.py
@@ -0,0 +1,9 @@
+"""
+Question:
+Write a program which can filter() to make a list whose elements are even number between 1 and 20 (both included).
+
+"""
+
+
+evenNumbers = filter(lambda x: x % 2 == 0, range(1, 21))
+print(evenNumbers)
