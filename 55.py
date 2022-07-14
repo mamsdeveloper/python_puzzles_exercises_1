de31df3f7c0fa76ba8b14e0543637a4a1f4b7460
mamsdeveloper <belk1naalyona@yandex.ru>
2022-07-14 14:58:10 +0600

    55.py solved

---
+++ 55.py
@@ -0,0 +1,20 @@
+"""
+Question:
+
+Write a function to compute 5/0 and use try/except to catch the exceptions.
+
+"""
+
+
+def throws():
+    return 5/0
+
+
+try:
+    throws()
+except ZeroDivisionError:
+    print("division by zero!")
+except Exception, err:
+    print('Caught an exception')
+finally:
+    print('In finally block for cleanup')
