f6de93c4cca18dc98c32a6afc1832faf37c3ef87
mamsdeveloper <belk1naalyona@yandex.ru>
2022-07-15 00:11:00 +0600

    56.py solved

---
+++ 56.py
@@ -0,0 +1,19 @@
+"""
+Question:
+Define a custom exception class which takes a string message as attribute.
+
+"""
+
+
+class MyError(Exception):
+    """My own exception class
+
+    Attributes:
+        msg  -- explanation of the error
+    """
+
+    def __init__(self, msg):
+        self.msg = msg
+
+
+error = MyError("something wrong")
